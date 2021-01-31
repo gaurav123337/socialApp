28fa434e69d83ca9ed60e4443c4268a8901c243e
Gaurav <gaurav.123337@gmail.com>
2021-01-31 16:32:44 +0530

    server setup

--- index.js
+++ index.js
@@ -1,23 +1,17 @@
-const {ApolloServer} = require('apollo-server');
-const gql = require('graphql-tag');
+const {ApolloServer, PubSub} = require('apollo-server');
+
 const mongoose = require('mongoose');
 
+const typeDefs = require('./graphql/typeDefs');
+const resolvers = require('./graphql/resolvers');
 const {MONGODB} = require('./config.js');
 
-const typeDefs= gql`
-  type Query{
-    sayHi:String!
-  }
-`
-const resolvers = {
-  Query:{
-    sayHi: () => 'Hello World!'
-  }
-}
+const pubsub = new PubSub();
 
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
+  context: ({req}) => ({req, pubsub})
 });
 
 mongoose.connect(MONGODB, {useNewUrlParser: true})
