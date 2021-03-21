b4d6c01588671415a8cd0453c586530c18735723
Gaurav <gaurav.123337@gmail.com>
2021-03-21 19:46:32 +0530

    backend configuration

--- index.js
+++ index.js
@@ -1,24 +1,27 @@
-const {ApolloServer, PubSub} = require('apollo-server');
+const { ApolloServer, PubSub } = require('apollo-server');
 
 const mongoose = require('mongoose');
 
 const typeDefs = require('./graphql/typeDefs');
 const resolvers = require('./graphql/resolvers');
-const {MONGODB} = require('./config.js');
+const { MONGODB } = require('./config.js');
 
 const pubsub = new PubSub();
-
+const PORT = process.env.port || 5000;
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context: ({req}) => ({req, pubsub})
+  context: ({ req }) => ({ req, pubsub })
 });
 
-mongoose.connect(MONGODB, {useNewUrlParser: true})
+mongoose.connect(MONGODB, { useNewUrlParser: true })
   .then(() => {
     console.log('Mongodb connected');
-    return server.listen({port: 5000})
+    return server.listen({ port: PORT })
   })
   .then(res => {
     console.log(`Server started at ${res.url}`)
+  })
+  .catch(err => {
+    console.log(err)
   })
